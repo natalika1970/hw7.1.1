20b19a9a8c20c0dc8d7f2329d5a82cc97bc1ab9d
natalika1970 <natalika_70@mail.ru>
2022-10-10 13:32:36 +0300

    изменила hw7.1.1

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,20 @@
+import java.io.OptionalDataException;
 import java.util.Arrays;
 import java.util.Random;
 
-public class Main {
+ public class Main {
+
+
     public static void main(String[] args) {
+
         int[] arr = generateRandomArray();
         System.out.println(Arrays.toString(arr));
+        int[] expenses = generateRandomArray();
+        System.out.println(Arrays.toString(expenses));
+        return 0;
     }
 
-    private static int[] arr() {
-        return new int[30];
-    };
+
     public static int[] generateRandomArray() {
 
         Random random = new Random();
@@ -19,13 +24,46 @@ public class Main {
         for (int i = 0; i < arr.length; i++) {
             arr[i] = random.nextInt(100000) + 100000;
         }
-        /*for (int index = 0; index < arr.length; index++) {
-            if (index == arr.length -1){
-                System.out.println(arr[index]);
-                break;
-            }
-            System.out.println((arr[index]+ " "));
-        }*/
         return arr;
     }
+    public static int[] expenses() {
+        System.out.println("задание 1 ");
+        int sum = 0;
+
+        int[] expenses = new expenses[0];
+        for (int expense : expenses) {
+            sum += expense;
+        }
+        System.out.println("сумма трат за месяц составила" + sum + " рублей");
+
+        System.out.println("задание 2 ");
+        int max = Integer.MIN_VALUE;
+        int mix = Integer.MAX_VALUE;
+        int min = 0;
+        for (int expense : expenses) {
+            if (expense > max) {
+                max = expense;
+            }
+            min = 0;
+            if (expense < min) {
+                min = expense;
+            }
+        }
+        System.out.println("максимальная сумма трат за день составила " + min);
+        System.out.println("минимальная сумма трат за день составила " + max);
+        return new int[0];
+    }
+
+     System.out.println("задание 3 ");
+    double averageExpense = (double) i / expenses.length;
+      System.out.println("средняя сумма трат за месяц составила" + averageExpense + " рублей");
+
+
+       System.out.println("задание 4 ");
+    char[] reverseFullName = {'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
+              for (int i = reverseFullName.length - 1; i >= 0; i--) {
+        System.out.println(reverseFullName[i]);
+    }
+
+
 }
